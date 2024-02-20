bfc9da151e25ce3a7a0508e2058c3dc6d339c69b
syo <syo093c@gmail.com>
2024-02-21 01:36:48 +0900

    update

--- scripts/base/15-kernel.sh
+++ scripts/base/15-kernel.sh
@@ -3,5 +3,9 @@
 sed -i -e 's/^HOOKS=(base udev/HOOKS=(base asahi udev/' \
 	/etc/mkinitcpio.conf
 
+# btrfs
+sed -i -e 's/^MODULES=(/MODULES=(btrfs/' \
+	/etc/mkinitcpio.conf
+
 mkdir -p /boot/efi/m1n1
 pacman --noconfirm -S linux-asahi asahi-meta
